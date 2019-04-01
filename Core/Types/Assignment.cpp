e3911d57afad494a52b6c0622a10f11c5667e7d3
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-04-01 12:51:31 +0200

    improved Assignment (toString, vector as member, separation of implementation)

---
+++ Core/Types/Assignment.cpp
@@ -0,0 +1,57 @@
+#include "stdafx.h"
+#include "Assignment.h"
+
+#include <string>
+
+Assignment::Assignment(Variable numberOfVariables, VariableState state)
+    :
+    states(std::vector<VariableState>(numberOfVariables, state))
+{
+
+}
+
+bool Assignment::IsSAT(Literal literal) const
+{
+    Variable var = ToVariable(literal);
+    bool positive = literal == var;
+    if (positive) {
+        return states[var] == VariableState::True;
+    } else {
+        return states[var] == VariableState::False;
+    }
+}
+
+VariableState Assignment::GetState(Variable variable) const
+{
+    return states[variable];
+}
+
+void Assignment::SetState(Variable variable, VariableState state)
+{
+    states[variable] = state;
+}
+
+size_t Assignment::Size() const
+{
+    return states.size();
+}
+
+std::ostream& operator<<(std::ostream& out, const Assignment& a)
+{
+    for (decltype(a.Size()) i = 0; i < a.Size(); i++) {
+        switch (a.GetState(i)) {
+            case VariableState::Undefined:
+                out << '?' << i;
+                break;
+            case VariableState::False:
+                out << '-';
+                // falltrough intended
+            case VariableState::True:
+                out << i;
+                break;
+            default:
+                throw std::runtime_error("illegal state of variable");
+        }
+    }
+    return out;
+}
--- Core/Types/Assignment.h
+++ Core/Types/Assignment.h
@@ -3,6 +3,7 @@
 #include <vector>
 
 #include "DLLMakro.h"
+#include "Types/Literal.h"
 
 enum class VariableState : char {
     Undefined = -1,
@@ -13,21 +14,34 @@ enum class VariableState : char {
 template class CORE_API std::allocator<VariableState>;
 template class CORE_API std::vector<VariableState>;
 
-class CORE_API Assignment : public std::vector<VariableState> {
+class CORE_API Assignment {
+private:
+    std::vector<VariableState> states;
+public:
+    Assignment(Variable numberOfVariables, VariableState state = VariableState::Undefined);
 public:
     /// <summary>
     /// Unchecked.
     /// </summary>
     /// <param name="literal"></param>
     /// <returns></returns>
-    bool IsTrue(Literal literal) const
-    {
-        Variable var = ToVariable(literal);
-        bool positive = literal == var;
-        if (positive) {
-            return operator[](var) == VariableState::True;
-        } else {
-            return operator[](var) == VariableState::False;
-        }
-    }
+    bool IsSAT(Literal literal) const;
+
+    /// <summary>
+    /// Unchecked.
+    /// </summary>
+    /// <param name="variable"></param>
+    /// <returns></returns>
+    VariableState GetState(Variable variable) const;
+
+    /// <summary>
+    /// Unchecked.
+    /// </summary>
+    /// <param name="variable"></param>
+    /// <returns></returns>
+    void SetState(Variable variable, VariableState state);
+
+    size_t Size() const;
 };
+
+CORE_API std::ostream& operator<<(std::ostream& out, const Assignment& a);
