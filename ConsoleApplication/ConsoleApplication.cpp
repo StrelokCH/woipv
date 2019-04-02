61f9d7f09490db95a44f042e8153dae590cb18cb
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-04-02 12:41:18 +0200

    added draft of solving in main

--- ConsoleApplication/ConsoleApplication.cpp
+++ ConsoleApplication/ConsoleApplication.cpp
@@ -8,6 +8,7 @@
 
 #include "Core/Types/Assignment.h"
 #include "Core/Utility/CNFParser.h"
+#include "Core/Interfaces/SATSolver.h"
 
 int main(int argc, char *argv[])
 {
@@ -22,6 +23,39 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
+    // Todo: solve
     auto p = ParseCNF(infile);
     auto a = Assignment(5);
+
+    auto assignment = std::optional<Assignment>();
+    auto solvingResult = SolvingResult::Undefined;
+
+    std::ofstream outfile(argv[2]);
+    if (!outfile) {
+        std::cout << "Could not open output file (" << argv[2] << ").";
+        return EXIT_FAILURE;
+    }
+
+    // output result
+    switch (solvingResult) {
+        case SolvingResult::Satisfiable:
+            outfile << "sat";
+            break;
+        case SolvingResult::Unsatisfiable:
+            outfile << "unsat";
+            break;
+        case SolvingResult::Undefined:
+            outfile << "undef";
+            break;
+        default:
+            throw std::runtime_error("invalid SolvingResult");
+    }
+    outfile << std::endl;
+    if (assignment) {
+        outfile << assignment.value();
+    } else {
+        outfile << "not assignment";
+    }
+
+    return EXIT_SUCCESS;
 }
