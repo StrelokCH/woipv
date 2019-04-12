5a6c4002d5fda4fcd30fcaf052110f37e98859c0
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-04-12 13:50:58 +0200

    changed SetPartitionSolver to accept a shared_ptr instead of reference

--- Core/Interfaces/SATPartitioner.h
+++ Core/Interfaces/SATPartitioner.h
@@ -3,10 +3,19 @@
 #include "Core/DLLMakro.h"
 #include "SATSolver.h"
 
+#include <memory>
+
 /// <summary>
 /// Use this class to indicate a SATSolver that relies on SATPartitioning.
 /// </summary>
 class CORE_API SATPartitioner : public SATSolver {
+protected:
+    std::shared_ptr<SATSolver> partitionSolver;
+
 public:
-    virtual void SetPartitionSolver(SATSolver& solver) abstract;
+    /// <summary>
+    /// Inject solver which is used for solving partitions.
+    /// </summary>
+    /// <param name="solver"></param>
+    virtual void SetPartitionSolver(std::shared_ptr<SATSolver> solver);
 };
