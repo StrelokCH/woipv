db412747077e36d1b10e1ce078e83451185b7349
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-04-13 15:52:55 +0200

    fixed wrong iterator in SATSolver

--- Core/Interfaces/SATSolver.cpp
+++ Core/Interfaces/SATSolver.cpp
@@ -9,7 +9,7 @@ std::vector<Solution> SATSolver::Solve(const std::vector<Problem>& problems, Opt
     auto start = std::chrono::steady_clock::now();
 
     decltype(Solve(problems, timeLimit)) ret;
-    std::transform(problems.begin(), problems.end(), ret.begin(), [this, &start, &timeLimit](auto p) {
+    std::transform(problems.begin(), problems.end(), std::back_inserter(ret), [this, &start, &timeLimit](auto p) {
         return Solve(p, GetRemaining(timeLimit, start));
     });
     return ret;
