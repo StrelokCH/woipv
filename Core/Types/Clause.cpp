97283011febef17788ac0f50012b2b8033e1a068
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-04-26 16:24:05 +0200

    added operator<< for Clause

---
+++ Core/Types/Clause.cpp
@@ -0,0 +1,10 @@
+#include "Core/stdafx.h"
+#include "Clause.h"
+
+std::ostream& operator<<(std::ostream& out, const Clause& clause)
+{
+    for (auto& literal : clause) {
+        out << literal << " ";
+    }
+    out << 0;
+}
--- Core/Types/Clause.h
+++ Core/Types/Clause.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <ostream>
 
 #include "Core/DLLMakro.h"
 #include "Literal.h"
@@ -8,3 +9,5 @@
 template class CORE_API std::vector<Literal>;
 
 using Clause = std::vector<Literal>;
+
+CORE_API std::ostream& operator<<(std::ostream& out, const Clause& clause);
