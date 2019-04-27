602e4b46da1aa297d126d9ef8e543ff89ca69b34
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-04-27 21:28:23 +0200

    added skeleton of LocalSolver

---
+++ LocalSolver/LocalSolver.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "LocalSolver.h"
+
+std::pair<SolvingResult, std::optional<Assignment>> LocalSolver::Solve(const Problem & problem, OptionalTimeLimitMs timeLimit)
+{
+    return std::pair<SolvingResult, std::optional<Assignment>>();
+}
