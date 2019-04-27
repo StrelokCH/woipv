839c653828140fc09cf91dd0860e921a1ceffc7b
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-04-27 22:47:50 +0200

    implemented LocalSolverSat

--- LocalSolverSat/LocalSolverSat.cpp
+++ LocalSolverSat/LocalSolverSat.cpp
@@ -1,53 +1,102 @@
 #include "stdafx.h"
 #include "LocalSolverSat.h"
 
+#include <iostream>
+
 #include "LocalSolver/localsolver.h"
+
+using namespace localsolver;
+
+Assignment CreateAssignment(const std::vector<localsolver::LSExpression>& variables)
+{
+    Assignment assignment(static_cast<Variable>(variables.size() - 1));
+
+    Variable counter = 0;
+    for (auto variable : variables) {
+        if (counter == 0) {
+            counter++;
+            continue;
+        }
+
+        auto value = variable.getIntValue();
+        if (value == 1) {
+            assignment.SetState(counter, VariableState::True);
+        } else if (value == 0) {
+            assignment.SetState(counter, VariableState::False);
+        } else {
+            throw std::runtime_error("unknown variable value");
+        }
+        counter++;
+    }
+
+    return assignment;
+}
 
 std::pair<SolvingResult, std::optional<Assignment>> LocalSolverSat::Solve(const Problem & problem, OptionalTimeLimitMs timeLimit)
 {
-    try {
-        using namespace localsolver;
+    // Declares the optimization model.
+    LocalSolver localsolver;
+    LSModel model = localsolver.getModel();
 
-        lsint weights[] = {10, 60, 30, 40, 30, 20, 20, 2};
-        lsint values[] = {1, 10, 15, 40, 60, 90, 100, 15};
-        lsint knapsackBound = 102;
-
-        // Declares the optimization model.
-        localsolver::LocalSolver localsolver;
-        LSModel model = localsolver.getModel();
-
-        // 0-1 decisions
-        LSExpression x[8];
-        for (int i = 0; i < 8; i++)
-            x[i] = model.boolVar();
-
-        // knapsackWeight <- 10*x0 + 60*x1 + 30*x2 + 40*x3 + 30*x4 + 20*x5 + 20*x6 + 2*x7;
-        LSExpression knapsackWeight = model.sum();
-        for (int i = 0; i < 8; i++)
-            knapsackWeight += weights[i] * x[i];
-
-        // knapsackWeight <= knapsackBound;
-        model.constraint(knapsackWeight <= knapsackBound);
-
-        // knapsackValue <- 1*x0 + 10*x1 + 15*x2 + 40*x3 + 60*x4 + 90*x5 + 100*x6 + 15*x7;
-        LSExpression knapsackValue = model.sum();
-        for (int i = 0; i < 8; i++)
-            knapsackValue += values[i] * x[i];
-
-        // maximize knapsackValue;
-        model.maximize(knapsackValue);
-
-        // close model, then solve
-        model.close();
-
-        // Parameterizes the solver.
-        LSPhase phase = localsolver.createPhase();
-        phase.setTimeLimit(1);
-        localsolver.solve();
-
-    } catch (const std::exception& e) {
-        throw e;
+    // create variables
+    std::vector<LSExpression> variables;
+    variables.reserve(problem.GetNumberOfVariables() + 1);
+    variables.push_back(model.boolVar());
+    for (auto variable = FirstVariable; variable <= problem.GetNumberOfVariables(); variable++) {
+        variables.push_back(model.boolVar());
     }
 
-    return std::pair<SolvingResult, std::optional<Assignment>>();
+    // get expression for number of satisfied clauses
+    LSExpression numberOfSatClauses = model.sum();
+    for (auto clause : problem.GetClauses()) {
+        LSExpression expr = model.sum();
+        for (auto literal : clause) {
+            if (IsPositive(literal)) {
+                expr += variables[literal];
+            } else {
+                expr += (1 - variables[Negate(literal)]);
+            }
+        }
+
+        // Todo: verify usefulness of this constraint
+        model.addConstraint(expr == 1);
+
+        numberOfSatClauses += model.iif(expr >= 1, 1, 0);
+    }
+
+    // maximize number of sat clauses
+    model.maximize(numberOfSatClauses);
+
+    // close model, then solve
+    model.close();
+
+    // set bound, which is to satisfy all clauses
+    localsolver.getParam().setObjectiveBound(0, static_cast<lsint>(problem.GetClauses().size()));
+
+    // set time limit if needed
+    if (timeLimit) {
+        auto timeLimitSeconds = std::chrono::duration_cast<std::chrono::seconds>(timeLimit.value()).count();
+        if (timeLimitSeconds == 0) {
+            std::cout << "warning: minimal timelimit of CryptoMiniSatSolver is 1 second" << std::endl;
+            timeLimitSeconds = 1;
+        }
+        LSPhase phase = localsolver.createPhase();
+        phase.setTimeLimit(static_cast<int>(timeLimitSeconds));
+    }
+
+    localsolver.solve();
+
+    // check result
+    auto solutionStatus = localsolver.getSolution().getStatus();
+    switch (solutionStatus) {
+        case SS_Infeasible:
+        case SS_Inconsistent:
+            return {SolvingResult::Unsatisfiable, {}};
+        case SS_Feasible:
+            // not finished
+            return {SolvingResult::Undefined, {}};
+        case SS_Optimal:
+            return {SolvingResult::Satisfiable, CreateAssignment(variables)};
+    }
+    throw std::runtime_error("unknown solution status");
 }
