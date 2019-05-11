aef11d1bc66c88f9aaafa4e711b77c4168d4a549
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-05-11 15:03:05 +0200

    cleanup TimeLimit

--- Core/Utility/TimeLimit.cpp
+++ Core/Utility/TimeLimit.cpp
@@ -1,14 +1,18 @@
 #include "Core/stdafx.h"
 #include "TimeLimit.h"
 
-OptionalTimeLimitMs GetRemaining(OptionalTimeLimitMs timeLimit, std::chrono::time_point<std::chrono::steady_clock> start)
+std::chrono::nanoseconds GetElapsed(std::chrono::steady_clock::time_point start)
+{
+    return std::chrono::steady_clock::now() - start;
+}
+
+OptionalTimeLimitMs GetRemaining(OptionalTimeLimitMs timeLimit, std::chrono::steady_clock::time_point start)
 {
     if (!timeLimit) {
         return timeLimit;
     }
 
-    auto elapsed = start - std::chrono::steady_clock::now();
-    return std::chrono::duration_cast<std::chrono::milliseconds>(timeLimit.value() - elapsed);
+    return std::chrono::duration_cast<std::chrono::milliseconds>(timeLimit.value() - GetElapsed(start));
 
 }
 
@@ -18,6 +22,5 @@ bool HasRemaining(OptionalTimeLimitMs timeLimit, std::chrono::steady_clock::time
         return true;
     }
 
-    auto elapsed = std::chrono::steady_clock::now() - start;
-    return timeLimit.value() > elapsed;
+    return timeLimit.value() > GetElapsed(start);
 }
