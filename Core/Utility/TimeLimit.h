9ef0088f93866d18333da285f9cae18e6a3ff227
Janik Meyer <janik.meyer@students.fhnw.ch>
2019-05-11 15:40:28 +0200

    added GetElapsed to CORE_API

--- Core/Utility/TimeLimit.h
+++ Core/Utility/TimeLimit.h
@@ -7,5 +7,6 @@
 
 using OptionalTimeLimitMs = std::optional<std::chrono::milliseconds>;
 
+CORE_API std::chrono::nanoseconds GetElapsed(std::chrono::steady_clock::time_point start);
 CORE_API OptionalTimeLimitMs GetRemaining(OptionalTimeLimitMs timeLimit, std::chrono::time_point<std::chrono::steady_clock> start);
 CORE_API bool HasRemaining(OptionalTimeLimitMs timeLimit, std::chrono::steady_clock::time_point start);
